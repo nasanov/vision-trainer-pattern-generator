d25d29a13ed44ea36957ff36ce96918b85ef79e5
nasanov <asanoff.n@gmail.com>
2025-11-19 00:56:13 -0500

    Reposition the helper grid

--- src/App.jsx
+++ src/App.jsx
@@ -561,7 +561,8 @@ export default function A4Generator() {
               <div className="absolute inset-0 pointer-events-none opacity-10 no-print"
                    style={{
                      backgroundImage: `linear-gradient(${pageSettings.textColor} 1px, transparent 1px), linear-gradient(90deg, ${pageSettings.textColor} 1px, transparent 1px)`,
-                     backgroundSize: '10mm 10mm'
+                     backgroundSize: '10mm 10mm',
+                     backgroundPosition: '8.5mm 5mm'
                    }}>
                 {/* Center visual guide for grid mode */}
                 <div className="absolute top-1/2 left-1/2 w-4 h-4 -translate-x-1/2 -translate-y-1/2 border border-red-500 rounded-full opacity-50"></div>
